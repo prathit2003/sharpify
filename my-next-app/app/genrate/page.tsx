052392f559aaf6cb2c8a88ffa3e96fa1e21d6f68
prathit2003 <arcius777@gmail.com>
2025-08-04 23:42:05 +0530

    fixed tools and genrate page layout

--- my-next-app/app/genrate/page.tsx
+++ my-next-app/app/genrate/page.tsx
@@ -7,25 +7,30 @@ import GenrateImage from "@/components/sections/dashboard/genrate";
 import useUIStore from "../store/UIatom";
 export default function ChatLayout() {
   const { isMobileMenuOpen } = useUIStore();
+
   return (
-    <div className="h-[100vh] w-[100vw] bg-main flex flex-col p-6 space-y-6 overflow-hidden">
+    <div className="relative h-[100vh] w-[100vw] bg-main flex flex-col p-6 space-y-6 overflow-hidden">
       <Topbar />
-      {/* Sidebar + Main Content */}
+
+      {/* Main content container (Sidebar + GenrateImage) */}
       <div className="flex w-full h-full overflow-hidden space-x-6">
-        {/* Sidebar */}
+        {/* Desktop Sidebar */}
         <div className="hidden md:block w-[20vw] p-4">
           <Sidebar />
         </div>
-        {isMobileMenuOpen && (
-          <div className="w-[35vw] md:hidden z-20 h-full">
-            <Sidebar />
-          </div>
-        )}
+
         {/* Main Content */}
         <main className="w-full md:w-[75vw] h-full">
           <GenrateImage />
         </main>
       </div>
+
+      {/* Mobile Sidebar (absolute positioned) */}
+      {isMobileMenuOpen && (
+        <div className="absolute top-0 left-0 z-20 w-[70vw] h-full md:hidden bg-main shadow-xl p-4">
+          <Sidebar />
+        </div>
+      )}
     </div>
   );
 }
